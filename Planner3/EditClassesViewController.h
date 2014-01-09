4c8f88806856b6ec4f61334cac01b7a9db4f1eae
ngamby <noah@gamboafamily.com>
2014-01-09 08:24:59 -0800

    completed list management
    the list aspect of the code is complete in this version

---
+++ Planner3/EditClassesViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditClassesViewController.h
+//  Planner3
+//
+//  Created by Noah Gamboa on 10/23/13.
+//  Copyright (c) 2013 Noah Gamboa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditClassesViewController : UITableViewController
+
+@end
---
+++ Planner3/MainTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  MainTableViewController.h
+//  Planner3
+//
+//  Created by Noah Gamboa on 10/23/13.
+//  Copyright (c) 2013 Noah Gamboa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewController : UITableViewController
+
+
+@end
